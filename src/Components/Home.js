e39cde3e59d15eb74fa0fea25145f21c156eb925
quan <quanluis@protonmail.com>
2018-04-01 13:37:18 -0400

    Created the about component, finishing the contact info and then setting up react router.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,14 +3,14 @@ import React from 'react';
 class Home extends React.Component {
     render(){
         return(
-            <div className = 'home'>
+            <div className = 'header'>
                <h1>
                    Welcome
                </h1>
                <p>
                  Ahoy matey it seems you have crossed this here treasure site.
                  Built with the infamous React this creation was. <br/> So take a look 
-                 around enjoy the visit and dont forget to take ye Scurvy back with you
+                 around, enjoy the visit and dont forget to take ye Scurvy back with you
                  when ya leave.
                </p>
             </div>
