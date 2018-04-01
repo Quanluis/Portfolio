abefd75e292ecf663d7c7608eef9eba41da94592
quan <quanluis@protonmail.com>
2018-04-01 11:14:41 -0400

    Installed React, Created a simple Navbar component, getting ready for additional styles and installing react router next.

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return(
+          <div className = "hello" >
+               <div className="navWide">
+      <div className="wideDiv">
+          <a href="https://github.com/Quanluis">Github1</a>
+          <a href="https://www.freecodecamp.org/quanluis">freecodecamp</a>
+          <a href="">Link 3</a>
+        </div>
+        </div>
+        </div>
+        )
+    }
+}
+export default Navbar;
