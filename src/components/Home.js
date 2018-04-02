c3f160e255f6559d5fad04b28f9a1e23e5696086
quan <quanluis@protonmail.com>
2018-04-02 16:43:21 -0400

    added a picture of myself on the main page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ class Home extends React.Component {
     render(){
         return(
             <div className = 'header'>
+               <img src={'https://avatars1.githubusercontent.com/u/35511948?s=400&u=263141ea319ac11c1b3af1e07743a3894a71803e&v=4'} alt = 'Not showing up' />
                <h1>
                    Welcome
                </h1>
                <p>
-                 Ahoy matey it seems you have crossed this here treasure site.
-                 Built with the infamous React this creation was. <br/> So take a look 
-                 around, enjoy the visit and dont forget to take ye Scurvy back with you
-                 when ya leave.
+                 Hi i'm Luis and welcome to my custom website built with React! <br/> Please 
+                 take a look around and check my projects. <br/> Love to code with others and help anyone
+                 as much as i can. let me know if you have any questions.  
                </p>
             </div>
         )
