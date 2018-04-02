776f1a9a4b61e30d3d5c33e6c38857f07308a8cb
quan <quanluis@protonmail.com>
2018-04-02 16:57:39 -0400

    Styled the img in the home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ class Home extends React.Component {
     render(){
         return(
             <div className = 'header'>
+               <br/>
                <img src={'https://avatars1.githubusercontent.com/u/35511948?s=400&u=263141ea319ac11c1b3af1e07743a3894a71803e&v=4'} alt = 'Not showing up' />
                <h1>
                    Welcome
@@ -11,7 +12,7 @@ class Home extends React.Component {
                <p>
                  Hi i'm Luis and welcome to my custom website built with React! <br/> Please 
                  take a look around and check my projects. <br/> Love to code with others and help anyone
-                 as much as i can. let me know if you have any questions.  
+                 as much as i can. let me know if you have any questions!  
                </p>
             </div>
         )
