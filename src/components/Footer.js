d4520136c2419b216997c6f0ae3f3fe0cc2c829d
quan <quanluis@protonmail.com>
2018-04-03 14:13:10 -0400

    Added images to  the footer component and  placed it under the main page. added styling that i need.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 
 class Footer extends React.Component {
-    render(){
-        return(
-            <div>
-                
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="footer">
+        <li>
+          <img src={require("../img/React.png")} alt="cannot display" />
+        </li>
+        <li>
+          <img src={require("../img/nodejs.png")} alt="cannot display" />
+        </li>
+        <li>
+          <img
+            className="react"
+            src={require("../img/reactrouter.png")}
+            alt="cannot display"
+          />
+        </li>
+        <li>
+          <img src={require("../img/css3.png")} alt="cannot display" />
+        </li>
+      </div>
+    );
+  }
 }
 export default Footer;
